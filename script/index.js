469a0e0a1bca9031b0a245b54c5d7e263d1da1c2
MaximMorkovnik <maxim.morkovnik@gmail.com>
2022-05-18 13:07:03 +0300

    create ToDoList

---
+++ script/index.js
@@ -0,0 +1,170 @@
+// function App(todoModel, root) {
+//     this.model = todoModel;
+//     this.root = root;
+// }
+
+// App.prototype.init = function() {
+//     // 
+// }
+
+function renderTask(task) {
+    return `
+        <div class="content__block ${task.completed ? 'completed' : ''}" data-id="${task.id}">
+            <img class="content__text-icon" src="public/images/dot.png" alt="dot">
+            <div class="content__text">${task.title}</div>
+            <div class="content__item">
+                <div class="content__priority content__priority_${task.priority}">${task.priority}</div>
+                <div class="content__deadline">
+                    <img class="content__label" src="public/images/label.png" alt="label">
+                    <p class="content__data">${task.date}</p>
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input class="custom-checkbox" ${task.completed ? 'checked' : ''} type="checkbox">
+                        <span></span>
+                    </label>
+                </div>
+                <a class="content__link" href="#" data-action="remove">
+                    <img class="content__link-icon" src="public/images/garbage.svg" alt="trash" width="20">
+                </a>
+            </div>
+        </div>
+    `;
+}
+
+function renderTasks(tasks) {
+    const tasksToRender = [];
+
+    for (let i = 0; i < tasks.length; i++) {
+        const taskHTML = renderTask(tasks[i]);
+
+        tasksToRender.push(taskHTML);
+    }
+
+    return tasksToRender.join('');
+}
+
+const todoApp = new ToDoList([
+    new Task(
+        'Standup meeting with the team WAYUP',
+        PRIORITIES.LOW,
+        '2022-02-10'
+    ),
+    new Task(
+        'Order pizza for Granny tonight',
+        PRIORITIES.MEDIUM,
+        '2022-03-07'
+    ),
+    new Task(
+        'Design, Develop and Deploy Apps to Netlify for Clients',
+        PRIORITIES.HIGH,
+        '2022-01-05'
+    )
+]);
+
+// cписок задач
+const tasksList = document.querySelector('#tasks-list');
+
+function updateTasksList(filterValue) {
+    const stats = todoApp.getStats();
+
+    for (let key in stats) {
+        const statHTML = document.getElementById(`stats-${key}`);
+
+        statHTML.querySelector('span').innerText = stats[key];
+    }
+
+    tasksList.innerHTML = renderTasks(todoApp.getTasksList(filterValue));
+}
+
+tasksList.addEventListener('click', function (event) {
+    const idAttribute = 'data-id';
+
+    if (event.target.getAttribute('data-action') === 'remove' || event.target.closest('[data-action="remove"]')) {
+        event.preventDefault();
+
+        const id = event.target.closest('[data-id]').getAttribute('data-id');
+
+        todoApp.removeTask(id);
+    } else {
+        let id = event.target.getAttribute(idAttribute);
+
+        if (!id) {
+            id = event.target.closest('[data-id]').getAttribute(idAttribute);
+        }
+
+        todoApp.toggle(id);
+    }
+
+    updateTasksList(searchField.value);
+});
+
+updateTasksList();
+
+// фильтр
+const searchField = document.getElementById('search');
+
+searchField.addEventListener('input', function () {
+    updateTasksList(searchField.value);
+});
+
+
+// модальное окно
+
+const modalTrigger = document.getElementById('new-task');
+
+function popupHandler(selector) {
+    const modal = document.querySelector(selector);
+
+    const controls = {
+        open() {
+            modal.classList.add('open');
+        },
+        close() {
+            modal.classList.remove('open');
+        }
+    };
+
+    return controls;
+}
+
+const newTaskModal = popupHandler('#modal');
+
+modalTrigger.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    newTaskModal.open();
+});
+
+document.querySelector('.modal').addEventListener('click', newTaskModal.close);
+
+document.querySelector('.task').addEventListener('click', e => e.stopPropagation());
+
+document.querySelector('#clear').addEventListener('click', () => {
+    todoApp.clear();
+
+    updateTasksList(searchField.value);
+});
+
+const newTaskForm = document.querySelector('#new-task-form');
+
+newTaskForm.addEventListener('submit', function () {
+    event.preventDefault();
+
+    const formElements = newTaskForm.elements;
+
+    todoApp.addTask(
+        new Task(
+            formElements.title.value,
+            PRIORITIES[formElements.priority.value],
+            formElements.date.value
+        )
+    );
+
+    formElements.title.value = '';
+    formElements.date.value = '';
+    formElements.priority.value = 0;
+
+    newTaskModal.close();
+    updateTasksList(searchField.value);
+});
---
+++ script/todo.js
@@ -0,0 +1,105 @@
+const PRIORITIES = {
+    LOW: 'low',
+    HIGH: 'high',
+    MEDIUM: 'medium'
+}
+function Task(title, priority, date) {
+    this.title = title;
+    this.priority = priority;
+    this.date = date;
+
+    this.id = String(Math.random()).slice(2, 9);
+    this.completed = false;
+
+    // this.complete = function() {
+    //     this.completed = true;
+    // }
+}
+
+Task.prototype.toggle = function () {
+    this.completed = !this.completed;
+}
+
+Task.prototype.isCompleted = function () {
+    return this.completed;
+}
+function ToDoList(initialTasks) {
+    this.tasks = initialTasks;
+}
+
+// То же что и:
+// this.addTask = function() {
+
+// }
+// только используя наследование
+ToDoList.prototype.addTask = function (task) {
+    this.tasks.push(task);
+}
+
+ToDoList.prototype.findTaskById = function (id) {
+    // return this.tasks.find(t => t.id === id);
+    for (let i = 0; i < this.tasks.length; i++) {
+        if (this.tasks[i].id === id) {
+            return {
+                task: this.tasks[i],
+                index: i
+            };
+        }
+    }
+
+    return {
+        task: null,
+        index: -1
+    };
+}
+
+ToDoList.prototype.removeTask = function (id) {
+    const currentTaks = this.findTaskById(id);
+
+    this.tasks.splice(currentTaks.index, 1);
+}
+
+ToDoList.prototype.toggle = function (id) {
+    const currentTaks = this.findTaskById(id);
+
+    currentTaks.task.toggle();
+}
+
+ToDoList.prototype.getTasksList = function (filter) {
+    if (!filter) {
+        return this.tasks;
+    }
+    // return this.tasks.filter(t => t.title.includes(filter));
+    const result = [];
+
+    for (let i = 0; i < this.tasks.length; i++) {
+        if (this.tasks[i].title.toLowerCase().includes(filter.toLowerCase())) {
+            result.push(this.tasks[i]);
+        }
+    }
+
+    return result;
+}
+
+ToDoList.prototype.getStats = function () {
+    const allTasks = this.tasks.length;
+    let completed = 0;
+
+    for (let i = 0; i < allTasks; i++) {
+        if (this.tasks[i].isCompleted()) {
+            completed++;
+        }
+    }
+
+    const pending = allTasks - completed;
+
+    return {
+        total: allTasks,
+        completed,
+        pending,
+    }
+}
+
+ToDoList.prototype.clear = function () {
+    this.tasks.length = 0;
+}
